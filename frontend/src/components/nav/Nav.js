6ad88cb07cb11373b3480c57c3a6c300e976a918
Muri <murimoro67@gmail.com>
2024-11-18 22:57:08 +0200

    Added nav module

---
+++ frontend/src/components/nav/Nav.js
@@ -0,0 +1,11 @@
+import styles from './Nav.module.css';
+
+const Nav = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>CAN Bus Simulator</h1>
+        </div>
+    );
+}
+
+export default Nav;
